8a44f61577b3716d8d3153cdcba2467b683a94eb
Andrew Lee <andyrewlee@gmail.com>
2015-10-29 16:32:28 -0700

    Finalize 30 assignments

--- 26_selection_sort/SelectionSort.java
+++ 26_selection_sort/SelectionSort.java
@@ -4,10 +4,12 @@ public class SelectionSort
 {
   public static void main(String[] args)
   {
-    int[] bigO = new int[] {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    int[] bigOmega = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int[] bigO = new int[] {7, 6, 5, 4, 3, 2, 1};
+    int[] bigOmega = new int[] {1, 2, 3, 4, 5, 6, 7};
 
+    System.out.println("BIG O");
     selectionSort(bigO);
+    System.out.println("BIG OMEGA");
     selectionSort(bigOmega);
   }
 
